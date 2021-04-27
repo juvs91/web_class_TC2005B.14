ad20758fcc0159180bf53a8c25b93323ecc1e439
thehaiwave <manueld0293@gmail.com>
2021-04-26 23:46:39 -0500

    Added req functionalities

--- javascript_intro/events.js
+++ javascript_intro/events.js
@@ -1,10 +1,33 @@
-let add_item_to_list = (dom_element_creator, dom_element_adder) => {
+let kpi_measures = []
+
+let calc_average = (array) => {
+  let sum = array.reduce((a, b) => 
+    a + b, 0
+  )
+
+  return sum/array.length
+}
+
+let calc_total = (arr) => {
+  return arr.reduce((a, b) => a + b, 0)
+}
+
+let add_item_to_list = (dom_element_creator, dom_element_adder, employees) => {
    return (_event) => {
     let employee_name = document.querySelector(".employe-name-input").value
     let kpi_measure = document.querySelector(".kpi-measure-input").value
     let dom_element = dom_element_creator(employee_name, kpi_measure)
     dom_element_adder(dom_element)
+
+    document.querySelector('.delete').addEventListener('click', delete_element_from_list)
+    kpi_measure.push(parseInt(kpi_measure, 10))
+    write(calc_total(kpi_measures), calc_average(kpi_measures))
    }
+}
+
+let delete_element_from_list = (total, average) => {
+  document.getElementById("total").innerHTML = total
+  document.getElementById("average").innerHTML = average
 }
 
 let string_dom_element_creator = (employee_name, measure) => {
@@ -29,6 +52,9 @@ let template_dom_element_adder = (node_dom_element) => {
   let list = document.querySelector("ul.item-list")
   return list.insertAdjacentElement('afterbegin', node_dom_element)
 }
+
+
+
 
 export {
   add_item_to_list,
