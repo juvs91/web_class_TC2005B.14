a945fe5d8290992d3d58c584d1f7266857ca26f8
Alexia <a01283646@itesm.mx>
2021-04-26 21:59:34 -0500

    Small Kpi Calculator Equipo 2

--- javascript_intro/events.js
+++ javascript_intro/events.js
@@ -1,15 +1,61 @@
-let add_item_to_list = (dom_element_creator, dom_element_adder) => {
+//Array de Kpi_measures
+let measures = new Array();
+//Función de Average
+function average(arr){
+  let sum = arr.reduce((acc, cur) => acc + cur, 0)
+  let answer = sum / arr.length
+  return answer
+}
+//Función de Total
+const arrSum = arr => arr.reduce((a,b) => a + b, 0)
+
+let add_item_to_list = (dom_element_creator, dom_element_adder, employees) => {
    return (_event) => {
     let employee_name = document.querySelector(".employe-name-input").value
     let kpi_measure = document.querySelector(".kpi-measure-input").value
     let dom_element = dom_element_creator(employee_name, kpi_measure)
     dom_element_adder(dom_element)
+
+    //Agrego un evento al boton de DELETE
+    document.querySelector('.delete').addEventListener('click', delete_element)
+    //Agrego el elemento al array
+    measures.push(parseInt(kpi_measure, 10))
+    //Calcula el Total y Average
+    write(arrSum(measures), average(measures)) 
+    console.log(measures)
    }
 }
 
-let string_dom_element_creator = (employee_name, measure) => {
-  return `<li><span class="employe-name">${employee_name}</span><span class="kpi-measure">${measure}</span><button class="delete"> delete</button></li>`
+//Función para eliminar un elemento 
+let delete_element=(event)=>{
+  const button = event.target;
+  const li = button.parentNode;
+  const ul = li.parentNode;
+  ul.removeChild(li);
+
+  //Elimina el elemento del array
+  let kpi_measure = li.querySelector(".kpi-measure").textContent
+  console.log(kpi_measure)
+  let pos = measures.indexOf(parseInt(kpi_measure, 10))
+  console.log(pos)
+  measures.splice(pos,1)
+  console.log(measures)
+  //Calcula el Total y Average
+  write(arrSum(measures), average(measures)) 
 }
+
+
+//Escribe el Total y Average en el DOM
+let write = (total, average) => {
+  document.getElementById("Total").innerHTML = total
+  document.getElementById("Average").innerHTML = average
+}
+
+
+let string_dom_element_creator = (employee_name, measure) => {
+  return `<li><span class="employe-name">${employee_name}</span><span class="kpi-measure">${measure}</span><button class="delete">delete</button></li>`
+}
+
 
 let template_dom_element_creator = (employee_name, measure) => {
   let element_list_mock = document.querySelector("template#item-mock").content.children[0]
@@ -29,6 +75,7 @@ let template_dom_element_adder = (node_dom_element) => {
   let list = document.querySelector("ul.item-list")
   return list.insertAdjacentElement('afterbegin', node_dom_element)
 }
+
 
 export {
   add_item_to_list,
